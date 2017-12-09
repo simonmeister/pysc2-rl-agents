3c96916531997208fd27471e3d5c64a065827eed
Simon Meister <simon.meister.93@gmail.com>
2017-12-09 11:01:58 +0100

    a2c WIP

---
+++ rl/dump_old_code.py
@@ -0,0 +1,38 @@
+# NON_BATCHED NUMPY VERSION
+# TODO finish; convert tf vars to lists and ints
+def sample_action(self, obs, policy, value):
+valid_actions = obs.observation['available_actions'] # TODO this is NOT BATCHED - can we ? no.. we can't batch this over environments, as each env at each step will have different acts and thus args
+# BUT WE CAN BATCH THE SAMPLING ITSELF!!
+a_0 = sample(policy['function_id']) # TODO use gather
+out_args = []
+for arg_type in actions.FUNCTIONS._func_list[a_0].args:
+  arg_name = ... # TODO find arg type name or index by type directly
+  pi = policy[arg_name]
+  a_l = [sample()]
+  if len(pi.): # map-dimensional
+    flat_pi = tf.reshape(pi, [-1, ])
+    flat_index = sample(flat_pi)
+    a_l = [flat_index % width, flat_index // height] # TODO height, width
+  out_args.append(a_l)
+return actions.FunctionCall(a_0, out_args)
+
+def sample_action_independent(valid_actions, policy):
+a_0 = sample(policy[0][valid_actions])
+out_args = []
+for arg_type in actions.FUNCTIONS._func_list[a_0].args:
+  is_spatial, pi = policy[1][arg_type]
+  if is_spatial:
+    height, width = pi.shape
+    flat_pi = tf.reshape(pi, [-1])
+    flat_index = sample(flat_pi)
+    a_l = [flat_index % width, flat_index // height]
+  else:
+    a_l = [sample(pi)]
+  out_args.append(a_l)
+
+return actions.FunctionCall(a_0, out_args)
+
+def step(sampling_fn, obs, policy, value):
+valid_actions = obs.observation['available_actions']
+policy_np, value_np = sess.run([policy, value], feed_dict=get_feed_dict(obs)) # TODO get_feed_dict
+action = sampling_fn(valid_actions, policy_np)
--- rl/networks/fully_conv.py
+++ rl/networks/fully_conv.py
@@ -43,10 +43,13 @@ class FullyConv():
     return layers.fully_connected(in, 256, activation_fn=tf.tanh)
 
   def non_spatial_output(self, in, channels):
-    return layers.fully_connected(in, channels, activation_fn=None)
+    logits = layers.fully_connected(in, channels, activation_fn=None)
+    return tf.nn.softmax(logits)
 
   def spatial_output(self, in):
-    return layers.conv2d(in, 1, kernel_size=1, stride=1, activation_fn=None)
+    logits = layers.conv2d(in, 1, kernel_size=1, stride=1, activation_fn=None)
+    logits = layers.flatten(logits)
+    return tf.nn.softmax(logits)
 
   def concat2d(self, lst):
     if self.data_format == 'NCHW':
@@ -106,25 +109,32 @@ class FullyConv():
 
 
 
-# TODO move this somewhere else
+# TODO move this somewhere else and re-organize the code
 # A2C
 
-# BATCHED VERSIONS
-def sample_independent(batch_valid_actions, policy):
-  masked_dn_pi = tf.gather_nd(policy[0], batch_valid_actions)
-  fn_samples = sample(masked_action_pi)
+def mask_invalid_actions(batch_valid_actions, fn_pi):
+  fn_pi *= batch_valid_actions
+  fn_pi /= tf.reduce_sum(fn_pi, axis=1, keep_dims=True)
+  return fn_pi
+
+def sample_independent(batch_valid_actions, policy, size):
+  fn_pi, arg_pis = policy
+  fn_pi = mask_invalid_actions(batch_valid_actions, fn_pi)
+  fn_samples = sample(fn_pi)
+
   arg_samples = dict()
-  for arg_type, arg_out in policy[1].items():
+  for arg_type, arg_out in arg_pis.items():
     is_spatial, arg_pi = arg_out
+    sampled_arg = sample(arg_pi)
     if is_spatial:
-      num_batch, height, width = tf.unstack(tf.shape(arg_pi))[:3]
-      flat_pi = tf.reshape(arg_pi, [num_batch, -1])
-      flat_index = sample(flat_pi)
-      arg_sample = [flat_index % width, flat_index // height]
+      height, width = size
+      arg_sample = [sampled_arg % width, sampled_arg // height]
     else:
-      arg_sample = [sample(arg_pi)]
+      arg_sample = [sampled_arg]
     arg_samples[arg_type] = arg_sample
-    return fn_samples, arg_samples
+
+  return fn_samples, arg_samples
+
 
 def step(sampling_fn, obs_list, policy, value):
   valid_actions_list = [obs.observation['available_actions'] for obs in obs_list]
@@ -142,47 +152,49 @@ def step(sampling_fn, obs_list, policy, value):
       a_l.append(arg_samples_np[arg_type])
     action = actions.FunctionCall(a_0, a_l)
     actions_list.append(action)
+
   return actions_list, values
 
-def update():
+
+def compute_total_log_probs(batch_valid_actions, policy, actions):
+  """Compute action log probabilities given predicted policies and selected
+  actions.
+
+  Args:
+    batch_valid_actions: one-hot (in last dimenson) tensor of shape
+      [num_batch, NUM_FUNCTIONS].
+    policy: [fn_pi, {arg_0: arg_0_pi, ..., arg_n: arg_n_pi}]], where
+      each value is a tensor of shape [num_batch, num_params] representing
+      probability distributions over the function ids or over discrete
+      argument values.
+    actions: [fn_ids, {arg_0: arg_0_ids, ..., arg_n: arg_n_ids}], where
+      each value is a tensor of shape [num_batch] representing the selected
+      argument or actions ids. The argument id will be -1 if the argument is
+      not available for a specific (state, action) pair.
+  """
+  def compute_log_probs(probs, labels):
+    return tf.log(tf.gather(probs, labels))
+
+  fn_id, arg_ids = actions
+  fn_pi, arg_pis = policy
+  fn_pi = mask_invalid_actions(batch_valid_actions, fn_pi)
+  fn_log_prob = compute_log_probs(fn_pi, fn_id)
+
+  # TODO logging for each arg_type
+  total = fn_log_prob
+  for arg_type in arg_type in actions.TYPES:
+    arg_id = arg_ids[arg_type]
+    arg_pi = arg_pis[arg_type]
+    arg_log_prob = compute_log_probs(arg_pi, tf.maximum(arg_id, 1e-10))
+    # Mask argument log prob if the argument is not relevant
+    arg_log_prob *= (arg_id != -1)
+    total += arg_log_prob
+
+  return total
+
+def compute_a2c_loss():
   pass
 
 
-  # NON_BATCHED NUMPY VERSION
-# TODO finish; convert tf vars to lists and ints
-def sample_action(self, obs, policy, value):
-  valid_actions = obs.observation['available_actions'] # TODO this is NOT BATCHED - can we ? no.. we can't batch this over environments, as each env at each step will have different acts and thus args
-  # BUT WE CAN BATCH THE SAMPLING ITSELF!!
-  a_0 = sample(policy['function_id']) # TODO use gather
-  out_args = []
-  for arg_type in actions.FUNCTIONS._func_list[a_0].args:
-    arg_name = ... # TODO find arg type name or index by type directly
-    pi = policy[arg_name]
-    a_l = [sample()]
-    if len(pi.): # map-dimensional
-      flat_pi = tf.reshape(pi, [-1, ])
-      flat_index = sample(flat_pi)
-      a_l = [flat_index % width, flat_index // height] # TODO height, width
-    out_args.append(a_l)
-  return actions.FunctionCall(a_0, out_args)
-
-def sample_action_independent(valid_actions, policy):
-  a_0 = sample(policy[0][valid_actions])
-  out_args = []
-  for arg_type in actions.FUNCTIONS._func_list[a_0].args:
-    is_spatial, pi = policy[1][arg_type]
-    if is_spatial:
-      height, width = pi.shape
-      flat_pi = tf.reshape(pi, [-1])
-      flat_index = sample(flat_pi)
-      a_l = [flat_index % width, flat_index // height]
-    else:
-      a_l = [sample(pi)]
-    out_args.append(a_l)
-
-  return actions.FunctionCall(a_0, out_args)
-
-def step(sampling_fn, obs, policy, value):
-  valid_actions = obs.observation['available_actions']
-  policy_np, value_np = sess.run([policy, value], feed_dict=get_feed_dict(obs)) # TODO get_feed_dict
-  action = sampling_fn(valid_actions, policy_np)
+def update():
+  pass
