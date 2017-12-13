fb6a9c1d652e33253435cea86b5b48a2673d1a48
Simon Meister <simon.meister.93@gmail.com>
2017-12-13 12:38:56 +0100

    add TODOs to run.py

--- run.py
+++ run.py
@@ -1,1 +1,5 @@
 import tensorflow as tf
+
+
+# TODO implement command line arguments (get some inspiration from sc2aibot)
+# TODO integrate code (runner + agent) to get full system (see sc2aibot run_agents.py)
